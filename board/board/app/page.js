684e074351331825a7c1c0beb173171792c590fa
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-08-21 22:17:04 +0900

    회원가입 페이지, API코드 작성

--- board/board/app/page.js
+++ board/board/app/page.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <Link href="/register">
+        <button>회원가입</button>
+      </Link>
       <div className="board">
         <Link href="/list">
           <h2>게시판</h2>{" "}
