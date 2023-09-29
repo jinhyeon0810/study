cc5bc8322f44f10e5680b1be653287d0817cf853
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-29 19:44:19 +0900

    테스트 코드 공부

---
+++ 테스트코드_공부/src/mock/check/test/check.test.js
@@ -0,0 +1,10 @@
+const check = require("../check");
+
+describe("check", () => {
+  let onSuccess;
+  let onFail;
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+});
